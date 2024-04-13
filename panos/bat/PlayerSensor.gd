18b093048e4b71918f0da9a258326bbfb0cd77b5
Panagiotis Oikonomou <opanagiotis@gmail.com>
2024-04-13 18:29:08 +0300

    hi

--- panos/bat/PlayerSensor.gd
+++ panos/bat/PlayerSensor.gd
@@ -12,4 +12,4 @@ func _on_body_entered(body: Node2D):
 		var agent = get_parent()
 		assert(agent.is_in_group("agent"), "Parent of sensor is not in agent group")
 		if agent.has_method("on_player_seen"):
-			agent.on_player_seen(body.global_position)
+			agent.on_player_seen(body as CharacterBody2D)
--- panos/bat/bat.gd
+++ panos/bat/bat.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+var player_ref: CharacterBody2D = null
+var seen := false
 
 
-func on_player_seen(player_position: Vector2) -> void:
-	print("I SEE YOU MOTHERFUCKER")
+func on_player_seen(player: CharacterBody2D) -> void:
+	player_ref = player
+	var player_pos = player.global_transform.origin
+	print("I SEE YOU ({0}, {1})".format(player_pos.x, player_pos.y))
 
-func _physics_process(delta):
+
+func _physics_process(delta) -> void:
 	pass
