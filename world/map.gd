9967aadb2c92c871ebcad681920a9a66359749e0
Panagiotis Oikonomou <opanagiotis@gmail.com>
2024-04-14 19:25:29 +0300

    improve level spawining, character spawning to be dynamic and wire up hud dependencies

--- world/map.gd
+++ world/map.gd
@@ -3,8 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player = Globals.player_scene.insantiate()	
+	var player = Globals.player_scene.instantiate()
 	player.global_position = $PlayerSpawnPoint.global_position
 	add_child(player)
 
-	
+func get_player() -> Player:
+	return $Player
