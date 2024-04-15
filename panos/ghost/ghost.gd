4e63ba9b4b4f72c2a97257c0145967312393396a
milos.matic1513@gmail.com <35368267+milosmatic1513@users.noreply.github.com>
2024-04-15 15:36:04 +0300

    Added variable projectiles and sprite flipping

--- panos/ghost/ghost.gd
+++ panos/ghost/ghost.gd
@@ -4,8 +4,6 @@ var player_ref: CharacterBody2D = null
 var seen := false
 var cooldown_left := 0
 
-const COOLDOWN := 500
-
 @onready var animation_player = $AnimationPlayer
 @onready var sprite = $Sprite2D
 @export var can_shoot_projectiles:bool = false
@@ -21,7 +19,7 @@ func die() -> void:
 	#queue_free()
 
 func attack(direction: Vector2):
-	const SPEED = 700
+	const SPEED = 300
 	var projectile = projectile_scn.instantiate()
 	projectile.global_position = global_position
 	projectile.fly_towards(direction, SPEED)
@@ -41,16 +39,20 @@ func _physics_process(delta) -> void:
 		flight_direction = -flight_direction
 	sprite.offset.y +=flight_direction*delta*flight_speed
 
-	if can_shoot_projectiles:
-		if seen and player_ref != null:
+	if seen and player_ref != null:
+		if player_ref.global_position.x > global_position.x:
+			sprite.flip_h=1
+		else:
+			sprite.flip_h=0
+		if can_shoot_projectiles:
 			cooldown_left -= delta
 			
 			var can_attack := false
 			if cooldown_left <= 0:
 				can_attack = true
-				cooldown_left = COOLDOWN
+				cooldown_left = randf_range ( 300, 700 )
 			
 			if can_attack:
 				var player_dir = (player_ref.global_position - global_position).normalized()
 				attack(player_dir)
-			
+				
